51d520633949744d74e7d89555054409b0c33395
coalman321 <coalman321@outlook.com>
2021-04-01 00:10:28 +0000

    fixed getValue function to properly report

--- src/interface/motorparser.cpp
+++ src/interface/motorparser.cpp
@@ -18,16 +18,16 @@ namespace robotmotors {
         for (TiXmlElement* motor = motors->FirstChildElement("motor"); motor != nullptr; motor = motor->NextSiblingElement("motor")) {
             //initialization work
             MotorMap motorMap = MotorMap();
-            bool fail[3] = {false, false, false};
+            bool success[3] = {false, false, false};
             std::string temp = "", temp2 = "";
             std::shared_ptr<std::map<std::string, double>> config = std::make_shared<std::map<std::string, double>>();
 
             //make sure required elements exist
-            fail[0] = getValue(motor, "topic", motorMap.topicName);
-            fail[1] = getValue(motor, "canid", temp);
-            fail[2] = getValue(motor, "type", temp2);
-            if (fail[0] || fail[1] || fail[2]) throw std::runtime_error(std::string("Motor definintion is missing required field(s): ") +
-                                                                        (fail[0] ? "topic " : "") + (fail[1] ? "canid " : "") + (fail[2] ? "type " : ""));
+            success[0] = getValue(motor, "topic", motorMap.topicName);
+            success[1] = getValue(motor, "canid", temp);
+            success[2] = getValue(motor, "type", temp2);
+            if (!(success[0] && success[1] && success[2])) throw std::runtime_error(std::string("Motor definintion is missing required field(s): ") +
+                                                                        (success[0] ? "topic " : "") + (success[1] ? "canid " : "") + (success[2] ? "type " : ""));
             motorMap.canID = std::stoi(temp);
             if(!getMotorType(temp2, motorMap.motorType)){
                 throw std::runtime_error("Specified motor type is invalid. Got " + temp2);
@@ -54,10 +54,10 @@ namespace robotmotors {
             TiXmlElement* feedback = motor->FirstChildElement("feedback");
             if (feedback) {
                 if (getValue(feedback, "rate", temp)) config->insert(std::pair<std::string, double>("feedback_rate", stob(temp) ? 1.0 : 0.0));
-                if (getValue(feedback, "position", temp)) config->insert(std::pair<std::string, double>("feedback_position", std::stod(temp)));
-                if (getValue(feedback, "velocity", temp)) config->insert(std::pair<std::string, double>("feedback_velocity", std::stod(temp)));
-                if (getValue(feedback, "current", temp)) config->insert(std::pair<std::string, double>("feedback_current", std::stod(temp)));
-                if (getValue(feedback, "voltage", temp)) config->insert(std::pair<std::string, double>("feedback_voltage", std::stod(temp)));
+                if (getValue(feedback, "position", temp)) config->insert(std::pair<std::string, double>("feedback_position", stob(temp) ? 1.0 : 0.0));
+                if (getValue(feedback, "velocity", temp)) config->insert(std::pair<std::string, double>("feedback_velocity", stob(temp) ? 1.0 : 0.0));
+                if (getValue(feedback, "current", temp)) config->insert(std::pair<std::string, double>("feedback_current", stob(temp) ? 1.0 : 0.0));
+                if (getValue(feedback, "voltage", temp)) config->insert(std::pair<std::string, double>("feedback_voltage", stob(temp) ? 1.0 : 0.0));
             }
 
             //handle current_limit element
@@ -91,19 +91,20 @@ namespace robotmotors {
      * @param elem the parent XML element
      * @param childName the name of the XML child element to find
      * @param value the resulting value of the child element.
-     * @return bool true if the element was not found or false if found
+     * @return bool true if the element was found or false if not found
      **/
     bool getValue(TiXmlElement* elem, const std::string& childName, std::string& value) {
         //std::cout << "checking for " << childName << std::endl;
 
         //make sure child element and corresponding text exists
         TiXmlElement* childElem = elem->FirstChildElement(childName);
-        if (!childElem) return true;
+        if (!childElem) return false;
+
         const char* xmlVal = childElem->GetText();
-        if (!xmlVal) return true;
+        if (!xmlVal) return false;
 
         value = std::string(xmlVal);
-        return false;
+        return true;
     }
 
     /**
